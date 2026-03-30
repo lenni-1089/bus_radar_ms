fa84b00120cb31a87becaaa1fa3843f42e11b39b
Lennart Rosenthal <rosenthal.lennart@gmail.com>
2026-03-30 18:11:47 +0200

    Fixed typo

--- kafka/config.py
+++ kafka/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 config = {
-     'bootstrap.servers': os.getenv('CONFLUENT_BOOTSRTRAP_SERVERS'),     
+     'bootstrap.servers': os.getenv('CONFLUENT_BOOTSTRAP_SERVERS'),     
      'security.protocol': os.getenv('CONFLUENT_SASL_SSL'),
      'sasl.mechanisms': os.getenv('CONFLUENT_CONNECTION_METHOD'),
      'sasl.username': os.getenv('CONFLUENT_USERNAME'), 
